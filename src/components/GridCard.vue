1f1f37437462ede1264228a7765019edd2de0468
Hugo Matheus <limabrot879@gmail.com>
2020-02-13 16:47:22 -0300

    chars walking...

---
+++ src/components/GridCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-card class="cardChar">
+      <q-card-section>
+        <div class="text-h4">{{ name }}</div>
+      </q-card-section>
+      <q-card-section>
+        <div class="text-h6">{{ birth_year }}</div>
+      </q-card-section>
+    </q-card>
+</template>
+
+<script>
+export default {
+  name: 'GridCard',
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+
+    birth_year: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
--- src/pages/Characters.vue
+++ src/pages/Characters.vue
@@ -1,11 +1,48 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>Olá, <strong>Personagens</strong>!</h1>
+  <q-page class="flex flex-center cards">
+    <GridCard
+      v-for="char in chars"
+      :key="char.name"
+      v-bind="char"
+    />
   </q-page>
 </template>
 
+<style scoped>
+  .cards {
+    flex-direction: column;
+  }
+
+  .cardChar {
+    width: 350px;
+    height: auto;
+    margin-top: 15px;
+    margin-bottom: 10px;
+  }
+</style>
+
 <script>
+import GridCard from 'components/GridCard'
+
+import axios from 'axios'
+
 export default {
-  name: 'pageCharacters'
+  name: 'pageCharacters',
+
+  components: {
+    GridCard
+  },
+
+  data () {
+    return {
+      chars: null
+    }
+  },
+
+  mounted () {
+    axios
+      .get('https://swapi.co/api/people/')
+      .then(res => (this.chars = res.data.results))
+  }
 }
 </script>
